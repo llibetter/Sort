0fe6204e8b28ad3758f603639d73536c17a08f59
lli <915133364@qq.com>
2019-10-22 23:42:44 +0800

    “修改项目代码”

--- ArraySort/Program.cs
+++ ArraySort/Program.cs
@@ -13,6 +13,7 @@ namespace ArraySort
             int[] test = new int[] {2,43,54,234,754,213,3,4,543,23,23,654,3,4,2341 };
             Console.WriteLine($"冒泡排序结果：\t{BubbleSort.Sort(test).Array2String()}");
             Console.WriteLine($"选择排序结果：\t{SectionSort.Sort(test).Array2String()}");
+            Console.WriteLine($"选择排序结果：\t{SectionSort.Sort(test).Array2String()}");
             Console.ReadKey();
         }
     }
