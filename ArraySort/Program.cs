18e14d2476df61d0dbbe09cff0d16f6f47c1d9dc
lli <915133364@qq.com>
2019-10-23 13:25:01 +0800

    修改输出样式

--- ArraySort/Program.cs
+++ ArraySort/Program.cs
@@ -12,32 +12,31 @@ namespace ArraySort
     {
         static void Main(string[] args)
         {
-            int[] test = new int[] {2,43,54,234,754,213,3,4,543,23,23,654,3,4,2341,-1 };
+            int[] test = new int[] { 2, 43, 54, 234, 754, 213, 3, 4, 543, 23, 23, 654, 3, 4, 2341, -1 };
             //int[] test = new int[] { 2, 3,1,5,9};
 
 
-            Stopwatch sw = new Stopwatch();
-            sw.Restart();
-            string s1 = BubbleSort.Sort(test).Array2String();
-            Console.WriteLine($"冒泡排序结果：\t{s1}");
-            sw.Stop();
-            Console.WriteLine($"耗时：\t{sw.Elapsed.TotalMilliseconds}ms");
-            
-            sw.Restart();
-            string s2 = SectionSort.Sort(test).Array2String();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Restart();
+            string str1 = BubbleSort.Sort(test).Array2String();
+            Console.Write($"冒泡排序结果：\t{str1}");
+            stopWatch.Stop();
+            Console.WriteLine($"\t耗时：{stopWatch.Elapsed.TotalMilliseconds}ms");
 
-            Console.WriteLine($"选择排序结果：\t{s2}");
-            sw.Stop();
-            Console.WriteLine($"耗时：\t{sw.Elapsed.TotalMilliseconds}ms");
+            stopWatch.Restart();
+            string str2 = SectionSort.Sort(test).Array2String();
+            Console.Write($"选择排序结果：\t{str2}");
+            stopWatch.Stop();
+            Console.WriteLine($"\t耗时：{stopWatch.Elapsed.TotalMilliseconds}ms");
 
-            sw.Restart();
-            string s3 = QuickSort.Sort(test).Array2String();
+            stopWatch.Restart();
+            string str3 = QuickSort.Sort(test).Array2String();
+            Console.Write($"快速排序结果：\t{str3}");
+            stopWatch.Stop();
+            Console.WriteLine($"\t耗时：{stopWatch.Elapsed.TotalMilliseconds}ms");
 
-            Console.WriteLine($"快速排序结果：\t{s3}");
-            sw.Stop();
-            Console.WriteLine($"耗时：\t{sw.Elapsed.TotalMilliseconds}ms");
 
-            if (s1 == s2 && s2 == s3)
+            if (str1 == str2 && str2 == str3)
                 Console.WriteLine("所有排序算法结果相同！");
 
 
