b8eb4d63597c860f9b7da0cca3888e0a367ca968
lli <915133364@qq.com>
2019-10-23 13:17:23 +0800

    添加快速排序

--- ArraySort/Program.cs
+++ ArraySort/Program.cs
@@ -1,19 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using ArraySort.SomeSorts;
 namespace ArraySort
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] test = new int[] {2,43,54,234,754,213,3,4,543,23,23,654,3,4,2341 };
-            Console.WriteLine($"冒泡排序结果：\t{BubbleSort.Sort(test).Array2String()}");
-            Console.WriteLine($"选择排序结果：\t{SectionSort.Sort(test).Array2String()}");
-            Console.WriteLine($"选择排序结果：\t{SectionSort.Sort(test).Array2String()}");
+            int[] test = new int[] {2,43,54,234,754,213,3,4,543,23,23,654,3,4,2341,-1 };
+            //int[] test = new int[] { 2, 3,1,5,9};
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            string s1 = BubbleSort.Sort(test).Array2String();
+            Console.WriteLine($"冒泡排序结果：\t{s1}");
+            sw.Stop();
+            Console.WriteLine($"耗时：\t{sw.Elapsed.TotalMilliseconds}ms");
+            
+            sw.Restart();
+            string s2 = SectionSort.Sort(test).Array2String();
+
+            Console.WriteLine($"选择排序结果：\t{s2}");
+            sw.Stop();
+            Console.WriteLine($"耗时：\t{sw.Elapsed.TotalMilliseconds}ms");
+
+            sw.Restart();
+            string s3 = QuickSort.Sort(test).Array2String();
+
+            Console.WriteLine($"快速排序结果：\t{s3}");
+            sw.Stop();
+            Console.WriteLine($"耗时：\t{sw.Elapsed.TotalMilliseconds}ms");
+
+            if (s1 == s2 && s2 == s3)
+                Console.WriteLine("所有排序算法结果相同！");
+
+
             Console.ReadKey();
         }
     }
--- ArraySort/SomeSorts/QuickSort.cs
+++ ArraySort/SomeSorts/QuickSort.cs
@@ -11,6 +11,49 @@ namespace ArraySort.SomeSorts
     /// </summary>
     class QuickSort
     {
+        public static int[] Sort(int[] inputSrc)
+        {
+            int[] input = inputSrc.ToArray();
+            if (input == null || input.Count() == 0)
+                return null;
+            ChildSort(input,0,input.Length-1);
+            return input;
+        }
 
+        private static void ChildSort(int[] input, int left, int right)
+        {
+            if (left >= right)
+                return;
+            int partitionIndex = Partition(input, left, right);
+            ChildSort(input, left, partitionIndex - 1);
+            ChildSort(input, partitionIndex + 1, right);
+        }
+        private static int Partition(int[] input, int left, int right)
+        {
+            
+            int low = left;
+            int high = right;
+            int pivot = input[low];
+            while (low < high)
+            {
+                while (low < high && input[high] >= pivot)
+                    high--;
+
+                input[low] = input[high];
+
+
+                while (low < high && input[low] <= pivot)
+                    low++;
+
+                input[high] = input[low];
+
+              
+
+
+            }
+
+            input[low] = pivot;
+            return low;
+        }
     }
 }
